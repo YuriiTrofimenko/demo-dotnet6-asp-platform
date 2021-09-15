44d5dcfdc5d9ecfb56c521098303335e0d15c55b
YuriiTrofimenko <tyaa@ukr.net>
2021-09-15 19:00:09 +0300

    functional custom middleware

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
+app.Use(async (context, next) =>
+    {
+        if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+        {
+            await context.Response.WriteAsync("Custom Middleware \n");
+        }
+        /* Console.WriteLine($"HTTP Method: {context.Request.Method}, HTTP Params:");
+        foreach (var keyValuePair in context.Request.Query)
+        {
+            Console.WriteLine($"{keyValuePair.Key} = {keyValuePair.Value}");
+        } */
+        await next();
+    }
+);
 
 app.MapGet("/", () => "Hello ASP.NET!");
 
